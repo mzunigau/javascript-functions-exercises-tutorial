f1a6a6693450d473143906d5b6d58735d7f76d2b
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-02-23 02:43:39 +0000

    ejercicio 9 faltan los demas

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -3,6 +3,7 @@ function calculateArea(length,edge)
 	return length * edge;
 }
 // Your code Below this line:
+console.log(calculateArea(4,4));
 
 
 
--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,8 @@
 // Your code goes here:
+function renderPerson(nombre, fecha, color, edad, genero){
+    return nombre +' is a '+ edad +' years old '+genero +' born on '+fecha +' with '+color +' eyes ';
 
+}
 
 
   ///*** Do not edit below this line ****/
